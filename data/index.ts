7067b9c13e71a8363b1144c0533f7d6d78e52a58
amomensah675 <amomensah675@gmail.com>
2024-07-14 01:39:05 +0000

    added Projects section

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,7 @@
+{/*{ name: "Testimonials", link: "#testimonials" } */}
 export const navItems = [
     { name: "About", link: "#about" },
     { name: "Projects", link: "#projects" },
-    { name: "Testimonials", link: "#testimonials" },
     { name: "Contact", link: "#contact" },
   ];
   
